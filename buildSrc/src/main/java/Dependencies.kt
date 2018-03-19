3fb71c63c1ad5ea0db72a25dfc7286db1c48c499
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-03-19 20:14:54 +0100

    Added buildSrc module

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,27 @@
+object Versions {
+    const val compileSdk = 27
+    const val minSdk = 17
+    const val targetSdk = 27
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val gradle = "3.0.1"
+    const val kotlin = "1.2.30"
+    const val support = "27.1.0"
+    const val junit = "4.12"
+    const val testRunner = "1.0.1"
+    const val expresso = "3.0.1"
+}
+
+object ClassPaths {
+    const val gradlePlugin = "com.android.tools.build:gradle:${Versions.gradle}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+}
+
+object Libs {
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
+    const val supportAppcompat = "com.android.support:appcompat-v7:${Versions.support}"
+    const val supportRecycler = "com.android.support:recyclerview-v7:${Versions.support}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val testRunner = "com.android.support.test:runner:${Versions.testRunner}"
+    const val espressoCore = "com.android.support.test.espresso:espresso-core:${Versions.expresso}"
+}
