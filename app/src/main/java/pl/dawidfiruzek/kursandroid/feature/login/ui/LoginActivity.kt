11c1fdecc6e9be46d3d954c5d0835ca945ed39cf
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-03-24 20:06:00 +0100

    Added layouts files

--- app/src/main/java/pl/dawidfiruzek/kursandroid/feature/login/ui/LoginActivity.kt
+++ app/src/main/java/pl/dawidfiruzek/kursandroid/feature/login/ui/LoginActivity.kt
@@ -1,6 +1,7 @@
 package pl.dawidfiruzek.kursandroid.feature.login.ui
 
 import android.os.Bundle
+import pl.dawidfiruzek.kursandroid.R
 import pl.dawidfiruzek.kursandroid.feature.commons.ui.BaseActivity
 import pl.dawidfiruzek.kursandroid.feature.login.LoginContract
 import javax.inject.Inject
@@ -10,8 +11,8 @@ class LoginActivity : BaseActivity(), LoginContract.View {
     @Inject
     lateinit var presenter: LoginContract.Presenter
 
-    override val layoutId: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val layoutId: Int =
+            R.layout.activity_login
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/pl/dawidfiruzek/kursandroid/feature/repositories/ui/RepositoriesActivity.kt
+++ app/src/main/java/pl/dawidfiruzek/kursandroid/feature/repositories/ui/RepositoriesActivity.kt
@@ -1,6 +1,7 @@
 package pl.dawidfiruzek.kursandroid.feature.repositories.ui
 
 import android.os.Bundle
+import pl.dawidfiruzek.kursandroid.R
 import pl.dawidfiruzek.kursandroid.feature.commons.ui.BaseActivity
 import pl.dawidfiruzek.kursandroid.feature.repositories.RepositoriesContract
 import javax.inject.Inject
@@ -10,8 +11,8 @@ class RepositoriesActivity : BaseActivity(), RepositoriesContract.View {
     @Inject
     lateinit var presenter: RepositoriesContract.Presenter
 
-    override val layoutId: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val layoutId: Int =
+            R.layout.activity_repositories
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
