77fb3ac37d4bb28d24c34f86474dd583913ab249
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-04-03 22:47:17 +0200

    Added injecting base url

--- app/src/main/java/pl/dawidfiruzek/kursandroid/feature/utils/configuration/StringConstants.kt
+++ app/src/main/java/pl/dawidfiruzek/kursandroid/feature/utils/configuration/StringConstants.kt
@@ -1,8 +1,10 @@
 package pl.dawidfiruzek.kursandroid.feature.utils.configuration
 
+import pl.dawidfiruzek.kursandroid.BuildConfig
+
 object StringConstants {
 
-    const val BASE_URL = "https://api.github.com/"
+    const val BASE_URL = BuildConfig.BASE_URL
 
     const val EXTRA_KEY_EXAMPLE = "extra_key_example"
 }
