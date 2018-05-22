fe68dac70d27732b3dba4b2c135d182037580f2c
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-05-22 11:27:09 +0200

    Added app injection

--- app/src/main/java/pl/dawidfiruzek/kursandroid/BaseApplication.kt
+++ app/src/main/java/pl/dawidfiruzek/kursandroid/BaseApplication.kt
@@ -1,16 +1,36 @@
 package pl.dawidfiruzek.kursandroid
 
+import android.app.Activity
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import io.fabric.sdk.android.Fabric
+import pl.dawidfiruzek.kursandroid.feature.utils.injection.DaggerAppComponent
+import javax.inject.Inject
 
-abstract class BaseApplication : Application() {
+abstract class BaseApplication : Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
         Fabric.with(this, Crashlytics())
         initTimber()
+        initDaggerComponent()
     }
 
     abstract fun initTimber()
+
+    private fun initDaggerComponent() {
+        DaggerAppComponent.builder()
+                .bindApplication(this)
+                .build()
+                .inject(this)
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> =
+            activityInjector
 }
